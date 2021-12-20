d94f75ded79a4acde38ae239505d9df093da2dc6
Lívia Siqueira <livia.siqueira12@outlook.com>
2021-12-20 00:52:28 -0300

    feat: arrumando bug no path mapping

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,17 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            "@components": "./src/components",
+            "@types": "./src/shared/types",
+            "@store": "./src/store"
+          }
+        }
+      ]
+    ]
   };
 };
