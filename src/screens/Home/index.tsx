ccbb57d8328876e9495dfb02d38ec04be546a933
Lívia Siqueira <livia.siqueira12@outlook.com>
2021-12-20 01:05:44 -0300

    feat: inicio paginá de Home, criação dos buttons de jogo

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Button, Text } from 'react-native'
 import { FlatList } from 'react-native-gesture-handler'
 import { useSelector } from 'react-redux'
 import { RootState } from '../../shared/types'
@@ -9,16 +9,16 @@ import * as styles from './styles'
 
 export const Home = () => {
     const userActualBets = useSelector((state: RootState) => state.user.userActual)
+    const games = useSelector((state: RootState) => state.game.games);
+    console.log(games)
+    const cart = [1, 2,3]
     return(
         <styles.Container>
            <Text>recent games</Text>
+         {cart.map((item) =>   
+             <Button title='test' onPress={() => {}}/>
+         )}
            <FlatList data={userActualBets?.bets} renderItem={item => <Text>{item}</Text>}/>
         </styles.Container>
     )
-}
-
-Home.navigationOptions = () => {
-    return {
-        headerTitle: 'TLG',
-    };  
 }
